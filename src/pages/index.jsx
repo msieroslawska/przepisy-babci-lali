88ccac369647d1d6ad06a0ab1913e43ce98a21f9
Marta <msieroslawska@users.noreply.github.com>
2020-10-05 20:29:26 +0200

    Init repo

---
+++ src/pages/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components"
+
+const PageWrapper = styled.div`
+  background-color: lavenderblush;
+`;
+
+export default function Home() {
+  return <PageWrapper>Hello world!</PageWrapper>;
+}
