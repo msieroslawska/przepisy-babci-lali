a8955bd72007f585101db9ad083f16d751441c44
Marta <msieroslawska@users.noreply.github.com>
2020-10-07 14:21:47 +0200

    Add 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { PageLayout } from "../components/pageLayout";
+
+const Default404: React.FC = () => (
+  <PageLayout header="404">
+    OH NOES! Strona nie istnieje :(
+  </PageLayout>
+);
+
+export default Default404;
