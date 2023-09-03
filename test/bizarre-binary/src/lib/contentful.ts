fae4fd8f30c3c0048d1a71ae39556cce811d2831
Marta <msieroslawska@users.noreply.github.com>
2023-09-03 21:05:49 +0200

    frankenstein evolves

--- test/bizarre-binary/src/lib/contentful.ts
+++ test/bizarre-binary/src/lib/contentful.ts
@@ -1,7 +1,31 @@
 import contentful from "contentful";
+import type { RecipeSkeleton } from "../types";
 
 export const contentfulClient = contentful.createClient({
   space: import.meta.env.CONTENTFUL_SPACE_ID,
   accessToken: import.meta.env.CONTENTFUL_DELIVERY_TOKEN,
   host: "cdn.contentful.com",
 });
+
+export const getAllRecipes = async () =>
+  contentfulClient.withoutUnresolvableLinks.getEntries<RecipeSkeleton>({
+    content_type: "recipe",
+  });
+
+type RecipeItems = Awaited<ReturnType<typeof getAllRecipes>>;
+
+export const mapRecipes = (recipes: RecipeItems) =>
+  recipes.items.map(recipe => {
+    const { image, title, slug } = recipe.fields;
+    return {
+      image,
+      title,
+      slug,
+      id: recipe.sys.id,
+    };
+  });
+
+export const getHero = async () =>
+  contentfulClient.withoutUnresolvableLinks
+    .getAsset("1BfFi4cIf3oUp66FUnMYhL")
+    .then(asset => `${asset.fields.file?.url}?w=1000&h=500`);
