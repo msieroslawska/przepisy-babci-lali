3f631f59036813138376eb8c1430e01a8285f0b3
Marta <msieroslawska@users.noreply.github.com>
2024-06-13 23:02:04 +0200

    Update stuff, remove react and so on

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
 
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [icon(), tailwind()],
   remotePatterns: [{ protocol: "https" }],
   domains: ["images.ctfassets.net"],
 });
