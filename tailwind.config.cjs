fbdf6cc77389980e1830bc199732a99b3fee9060
Marta <msieroslawska@users.noreply.github.com>
2024-06-14 16:14:44 +0200

    Improve the basic layout

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,5 +11,9 @@ module.exports = {
       },
     },
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  daisyui: {
+    themes: ["pastel"],
+    styled: true,
+  },
 };
