6c044334701453479c2b9b7d56ed73cb9bf5fecd
Marta <msieroslawska@users.noreply.github.com>
2024-06-21 22:54:45 +0200

    Update themes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -13,7 +13,7 @@ module.exports = {
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
   daisyui: {
-    themes: ["pastel"],
+    themes: ["nord"],
     styled: true,
   },
 };
